03770e2dd6cc1d9508ea89260b4d2037aa4895fb
Shivam Mourya <mouryashivam037@gmail.com>
2023-11-16 16:22:43 +0530

    commit of dependency injection

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModel;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;       
+        }
+        public async Task<CourseViewModel> GetCourse()
+        {
+            return new CourseViewModel()
+            {
+                Courses = await _courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly AppDbContext _context;
+        public CourseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Course>> GetCourses()
+        {
+          return await  _context.Course.ToListAsync();
+        }
+    }
+}
